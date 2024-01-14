cc3bff78fe7933596ceb4f74a2dd3b832181f7db
Nishant <nishantdongre30@gmail.com>
2024-01-14 23:46:00 +0530

    Solved LC -> 692. Top K Frequent Words and Lc -> 331. Verify Preorder Serialization of a Binary Tree

---
+++ Day 14/331. Verify Preorder Serialization of a Binary Tree.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    bool isValidSerialization(string preorder)
+    {
+        int nodeCnt = 0, nullCnt = 0;
+        vector<string> v = splitStr(preorder, ',');
+        for (int i = 0; i < v.size(); i++)
+        {
+            if (v[i] == "#")
+                ++nullCnt;
+            else
+                ++nodeCnt;
+            if (nullCnt >= nodeCnt + 1 && i != v.size() - 1)
+                return false;
+        }
+        return nullCnt == nodeCnt + 1;
+    }
+
+    vector<string> splitStr(string str, char delimiter)
+    {
+        vector<string> r;
+        string tmpstr;
+        while (!str.empty())
+        {
+            int ind = str.find_first_of(delimiter);
+            if (ind == -1)
+            {
+                r.push_back(str);
+                str.clear();
+            }
+            else
+            {
+                r.push_back(str.substr(0, ind));
+                str = str.substr(ind + 1, str.size() - ind - 1);
+            }
+        }
+        return r;
+    }
+};
