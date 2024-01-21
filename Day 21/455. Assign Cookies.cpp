93046d53f6167631163c97b69f20df507a48ab49
Nishant <nishantdongre30@gmail.com>
2024-01-22 00:07:10 +0530

    Solved LC -> 703. Kth Largest Element in a Stream and LC -> 455. Assign Cookies

---
+++ Day 21/455. Assign Cookies.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int findContentChildren(vector<int> &g, vector<int> &s)
+    {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int m = g.size(), n = s.size();
+        int i = 0, j = 0;
+
+        int res = 0;
+        while (i < m and j < n)
+        {
+            if (s[j] >= g[i])
+            {
+                res++;
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return res;
+    }
+};
